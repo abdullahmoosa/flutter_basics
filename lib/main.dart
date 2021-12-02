0c1b811b1929bd0eaa2f318c2a00777af648ddce
Abdullah <abdullahmoosa128@gmail.com>
2021-12-02 19:41:01 +0600

    Converted Stateful widget

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,31 @@ import 'package:flutter/material.dart';
 // }
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    // print('Answered!');
+    setState(() {
+      _questionIndex = _questionIndex +
+          1; // It is better to use this syntax rather than questionIndex++
+    });
+    // print(questionIndex);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var questions = [
+      'What\'s your favourite color?',
+      'What\'s your favorite animal?',
+    ];
     return MaterialApp(
       home: Scaffold(
           appBar: AppBar(
@@ -17,18 +39,26 @@ class MyApp extends StatelessWidget {
           ),
           body: Column(
             children: [
-              Text('The question!'),
+              Text(questions[_questionIndex]),
               RaisedButton(
                 child: Text('Answer 1'),
-                onPressed: null,
+                onPressed: _answerQuestion,
+                color: Colors.blue,
+                textColor: Colors.white,
               ),
               RaisedButton(
                 child: Text('Answer 2'),
-                onPressed: null,
+                onPressed: () => print('Answer 2 chosen'),
+                color: Colors.blue,
+                textColor: Colors.white,
               ),
               RaisedButton(
                 child: Text('Answer 3'),
-                onPressed: null,
+                onPressed: () {
+                  print('Answer 3 chosen!');
+                },
+                color: Colors.blue,
+                textColor: Colors.white,
               ),
             ],
           ) // body
